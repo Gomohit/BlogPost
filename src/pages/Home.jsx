10f0a6b8ed4925b2356e251ee59efd6c3697f925
Gomohit <goyalmohit2002@gmail.com>
2024-01-03 11:41:45 +0530

    solve the issue of post when logout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,7 @@ import { useNavigate,Link } from 'react-router-dom'
 function Home() {
     const navigate=useNavigate()
     const status=useSelector((state)=>state.auth.status)
+    console.log(status)
     const [posts,setPosts]=useState([])
 
     useEffect(()=>{
@@ -24,13 +25,14 @@ function Home() {
       
         fetchPosts();
     },[])
-    if(posts.length==0){
+    if(posts.length==0 || status==false){
         return (
                 <div className="w-full py-8  text-center">
                     <Container>
                         <div className="flex flex-wrap">
                             <div className="p-2 w-full mt-10 items-center flex justify-center">
                                 {status?(<div className='w-[400px] shadow-lg  border border-gray-100 p-5 flex flex-col items-center align-middle justify-center'>
+                                    {console.log('data is on')}
                                     <h1 className='w-full font-medium text-2xl mb-4'>You don&apos;t have post to see</h1>
                                     <h4 className=' mb-5'>Add Post From here
                                     </h4>
@@ -60,7 +62,7 @@ function Home() {
                                         2. If you don&apos;t have any account signup for create the account using basic details (name,email,password).
                                     </li>
                                     <li className='w-full text-xl text-left pb-4 font-sans'>
-                                        3. When you are login if posts are not shown so there is no previous data added by you, then you need to add the post which shown on your home page. 
+                                        3. When you are login and posts are not shown so there is no previous data added by you, then you need to add the post which shown on your home page. 
                                     </li>
                                 </ul>
                             </div>
@@ -69,8 +71,7 @@ function Home() {
     </div>
         )
     }
-    else{
-        return(
+    return(
             <div className='w-full py-8'>
                 <Container>
                     <h2 className='w-full text-xl font-serif text-left mb-5'>If you want to edit or delete the post click on post</h2>
@@ -85,6 +86,6 @@ function Home() {
             </div>
         )
     }    
-}
+
 
 export default Home
